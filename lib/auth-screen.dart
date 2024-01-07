c566fed830d03e596b29df6b4710fb3a6fefcbaf
ItsRamit <ece22145@iiitkalyani.ac.in>
2024-01-07 13:08:45 +0530

    UI design

--- lib/auth-screen.dart
+++ lib/auth-screen.dart
@@ -10,6 +10,46 @@ class auth_screen extends StatefulWidget {
 class _auth_screenState extends State<auth_screen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Todos"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: Container(
+        width: 400,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              width: 375,
+              height: 600,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: Colors.black, // Set your desired border color here
+                  width: 2.0, // Set the width of the border
+                ),
+                borderRadius: BorderRadius.circular(20),
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                height: 100,
+                width: 100,
+                color: Colors.green,
+                child: Center(
+                  child: Text("Fetch todos"),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
