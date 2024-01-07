7915d81a06dcfc91385407d757c9eaf1bf308f75
ItsRamit <ece22145@iiitkalyani.ac.in>
2024-01-07 12:56:32 +0530

    initial setup

---
+++ lib/auth-screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class auth_screen extends StatefulWidget {
+  const auth_screen({super.key});
+
+  @override
+  State<auth_screen> createState() => _auth_screenState();
+}
+
+class _auth_screenState extends State<auth_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
